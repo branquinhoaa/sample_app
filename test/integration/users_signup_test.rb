a354ada44a9555f686a8606935858616083324de
branquinhoaa <amanda.alves15@gmail.com>
2016-08-22 15:48:03 -0300

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  #By wrapping the post in the assert_no_difference method with the string argument ’User.count’, we arrange for a comparison between User.count before and after the contents inside the assert_no_difference block. 
+
+  test "the name should not be null" do
+    expected = CGI::escapeHTML('Name can\'t be blank')
+    post users_path, params:{
+      user:{
+        name: "",
+        email: "user@valid.com", password: "foobar", password_confirmation: "foobar"
+        }
+      } 
+    assert @response.body.include?(expected)
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+        email: "user@example.com",
+        password:              "password",
+        password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+test "invalid signup information" do
+  get signup_path
+  assert_no_difference 'User.count' do
+    post users_path, params: {
+      user: {
+        name: "",
+        email: "user@invalid",
+        password: "foo",
+        password_confirmation: "bar" 
+        }
+      }
+  end
+  assert_template 'users/new'
+end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,11 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "exampleUser", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "exampleUser", email: "user@example.com", password: "foobar", password_confirmation: "foobar") 
+
   end
+
+
 
   test "should be valid" do
     assert @user.valid?
@@ -60,12 +63,12 @@ foo@bar_baz.com foo@bar+baz.com]
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email    
   end  
-  
+
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " "*6
     assert_not @user.valid?
   end
-  
+
   test "password should have a minimun length" do
     @user.password = @user.password_confirmation = "a"*5
     assert_not @user.valid?
